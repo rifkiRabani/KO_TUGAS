fb32c3bc7d682228cc62fc54728dc060e9e3b41f
rifkiRabani <rifki.rabani93@gmail.com>
2026-08-31 00:42:35 +0700

    update report.bat

--- run_allure_report.bat
+++ run_allure_report.bat
@@ -1,5 +1,5 @@
 @echo off
 rmdir /s /q report\allure-results
 pytest tugas_5.py --alluredir=report/allure-results
-allure generate --single-file report/allure-results --name login-test --clean -o report/allure-report
+allure generate --single-file report/allure-results --name tugas_5 --clean -o report/allure-report
 pause
